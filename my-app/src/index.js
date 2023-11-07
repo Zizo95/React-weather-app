2355fad70471f9616b103017b651194fbb922a24
Zizo95 <zizo.brown.mbiko@gmail.com>
2023-11-07 06:14:05 +0200

    added open source

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -8,6 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <a href="https://github.com/Zizo95/React-weather-app">Open source by</a> Zizo Brown
   </React.StrictMode>
 );
 
