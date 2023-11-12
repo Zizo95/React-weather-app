5c21886e60c2a47309acfe3e01387440bd41b544
Zizo95 <zizo.brown.mbiko@gmail.com>
2023-11-12 12:07:42 +0200

    updated app.js

--- src/App.js
+++ src/App.js
@@ -5,8 +5,10 @@ import "bootstrap/dist/css/bootstrap.css";
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <h1>Weather App</h1>
       <Weather />
+      </div>
     </div>
   );
 }
