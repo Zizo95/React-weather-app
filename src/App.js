05785bcc267a5be1f1df09c9941697d7763d7ec6
Zizo95 <147083219+Zizo95@users.noreply.github.com>
2023-11-28 10:28:08 +0200

    Removed video

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ export default function App() {
     <div className="App">
       <div className="container">
       <h1>Weather App</h1>
-      <Weather defaultCity={"Durban"}/>
+      < Weather defaultCity={"Durban"}/>
       </div>
     </div>
   );
